7d5b9f10b954a357362b79b07a71a1235756bc48
1210476 <1210476@isep.ipp.pt>
2025-05-28 16:07:49 +0100

    PHP pagamento

--- pagamento.php
+++ pagamento.php
@@ -1,6 +1,29 @@
 <!DOCTYPE html>
 <html lang = "pt">
-<head>
+<body>
+
+<h1>Pagamento</h1>
 
 <?php
+$metodo = $_GET["metodo"];
+$telemovel = $_GET("telemovel"];
+$nome = $_GET("nome"];
+$numero = $_GET("numero"];
+$validade = $_GET("validade"];
+$cvv = $_GET("cvv"];
 
+echo "<p>Metodo de pagamento: $metodo</p>";
+
+   if ($metodo == "mbway") {
+	echo "<p>Telemóvel: $telemovel</p>";
+   }
+   else if ($metodo == "cartao") {
+	echo "<p>Nome no cartão: $nome</p>";
+	echo "<p>Número do cartão: $numero</p>";
+	echo "<p>Validade do cartão: $validade</p>";
+	echo "<p>CVV do cartão: $cvv</p>";
+   }
+?>
+
+</body>
+</html>
